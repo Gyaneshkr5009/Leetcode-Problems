b10f814e9fd10ef6c4feee463d2c7c76e40ce177
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-08-11 00:04:59 +0530

    Create Sudoku_solver.java

---
+++ Backtracking/Sudoku_solver.java
@@ -0,0 +1,62 @@
+class Solution {
+    public boolean isSafe(char[][] board,int row,int col,int num){
+        for(int i=0;i<board.length;i++){
+            if(board[i][col]==(char)(num+'0')){
+                return false;
+            }
+        }
+        for(int j=0; j<board.length; j++) {
+            if(board[row][j] == (char)(num+'0')) {
+               return false;
+            }
+        }
+
+        int srow=(row/3)*3;
+        int scol=(col/3)*3;
+        for(int i=srow;i<srow+3;i++){
+            for(int j=scol;j<scol+3;j++){
+                if(board[i][j]==(char)(num+'0')){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public void solveSudoku(char[][] board) {
+        helper(board,0,0);
+    }
+    public boolean helper(char[][] board,int row,int col){
+        if(row==board.length){
+            return true;
+        }
+        int nrow=0;
+        int ncol=0;
+        if(col!=board.length-1){
+            nrow=row;
+            ncol=col+1;
+        }
+        else{
+            nrow=row+1;
+            ncol=0;
+        }
+        if(board[row][col]!='.'){
+            if(helper(board,nrow,ncol)){
+                return true;
+            }
+        }
+        else{
+            for(int i=1;i<=9;i++){
+                if(isSafe(board,row,col,i)){
+                    board[row][col]=(char)(i+'0');
+                    if(helper(board,nrow,ncol)){
+                        return true;
+                    }
+                    else{
+                        board[row][col]='.';
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
