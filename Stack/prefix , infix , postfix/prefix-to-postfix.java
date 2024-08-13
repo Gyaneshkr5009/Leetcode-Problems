839a2c5c86c997c6660a0c6556da572b61a82339
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-08-13 19:53:17 +0530

    Create prefix-to-postfix.java

---
+++ Stack/prefix , infix , postfix/prefix-to-postfix.java
@@ -0,0 +1,29 @@
+ Time Complexity : O(n)
+  Space Complexity : O(n)
+
+***************************************************************************************************************************************
+// approach is we have to reverse the postfix to prefix conversion problem;
+  // so for achieving that we can simply follow some steps;
+   //step1: iterate from the last of string and add operands(like a,9,A) to the stack;
+   //step2: if we got operator we simply pop() two element from stack as top1 , and top2;
+   //step3: add in order top1 +top2 + str.charAt(i) ,then put new formed String back to Stack;
+   //step4: do this until the prefix string comes at the top of stack just return st.pop();
+
+class Solution {
+    static String preToPost(String s) {
+        if(s==null || s.length() == 0) return "";
+        Stack<String> st=new Stack<>();
+        String result="";
+        for(int i=s.length()-1 ; i>=0  ; i--){
+            char c=s.charAt(i);
+            if(Character.isLetterOrDigit(c)) st.push(String.valueOf(c));
+            else{
+                String top1=st.pop();
+                String top2=st.pop();
+                result=top1+top2+c;
+                st.push(result);
+            }
+        }
+        return st.pop();
+    }
+}
