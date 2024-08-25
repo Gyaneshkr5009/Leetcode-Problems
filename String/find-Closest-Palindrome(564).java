9ba8e926b95fe32aa9bf0673b8a7e60f1f863f29
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-08-25 15:54:00 +0530

    Update find-Closest-Palindrome(564).java

--- String/find-Closest-Palindrome(564).java
+++ String/find-Closest-Palindrome(564).java
@@ -47,3 +47,52 @@ class Solution {
         return ans;
     }
 }
+
+
+*****************************************c++*****************************************************************************8
+    class Solution {
+public:
+    long palindrome(long left, bool even) {
+        long resultNum = left;
+        if (!even) {
+            left = left / 10;
+        }
+
+        while (left > 0) {
+            int digit = left % 10;
+            resultNum = (resultNum * 10) + digit;
+            left /= 10;
+        }
+        return resultNum;
+    }
+    string nearestPalindromic(string n) {
+        int l=n.length();
+        int mid=l/2;
+        int firstHalfLength= (l%2==0)?mid:mid+1;
+        long firstHalf=stol(n.substr(0 , firstHalfLength));
+
+        vector<long> possibleSol; 
+        possibleSol.push_back(palindrome(firstHalf , l%2==0));      // fuctn that takes half of string and filling
+        possibleSol.push_back(palindrome(firstHalf + 1 , l%2==0));    //remaining t make palindrome;
+        possibleSol.push_back(palindrome(firstHalf - 1 , l%2==0));
+        possibleSol.push_back((long)pow(10 , l)+1);      // eq 999 so pow(10^3 + 1) | nearest will be 1001; 
+        possibleSol.push_back((long)pow(10 , l-1)-1);      // eq 101 so pow(10^(3-1) - 1) | nearest will be 99;
+
+        long diff=LONG_MAX;
+        long res=LONG_MAX;
+        long originalNum=stol(n);
+
+        for(long &num:possibleSol){
+            if(num==originalNum) continue;
+            if(abs(num-originalNum)<diff){
+                diff=abs(num-originalNum);
+                res=num;
+            }
+            else if(abs(num-originalNum)==diff){
+                res=min(res,num);
+            }
+        }
+
+        return to_string(res);
+    }
+};
