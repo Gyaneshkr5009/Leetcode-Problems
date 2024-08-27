b588760050f0fd2e79b539c5a08c33c8580c2e0f
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-08-27 20:56:35 +0530

    Create recerse_words(151).java

---
+++ String/recerse_words(151).java
@@ -0,0 +1,13 @@
+//time complexity:O(n)
+//space complexity:O(n)
+//******************************************************************************************************************************
+class Solution {
+    public String reverseWords(String s) {
+        String[] word=s.split("\\s+");
+        StringBuilder sb=new StringBuilder();
+        for(int i=word.length-1 ; i>=0 ; i--){
+            sb.append(word[i]+ " ");
+        }
+        return sb.toString().trim();
+    }
+}
