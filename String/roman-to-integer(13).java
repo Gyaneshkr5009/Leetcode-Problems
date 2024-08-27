fcf3ba225662afb3c51cd4d05fe247dae9d2e567
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-08-28 01:55:24 +0530

    Create roman-to-integer(13).java

---
+++ String/roman-to-integer(13).java
@@ -0,0 +1,26 @@
+//Time complexity: O(n);
+//Space complexity: O(1);
+
+//**********************************************************************************************************************************************
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character,Integer> mpp=new HashMap<>();
+        mpp.put('I',1);
+        mpp.put('V',5);
+        mpp.put('X',10);
+        mpp.put('L',50);
+        mpp.put('C',100);
+        mpp.put('D',500);
+        mpp.put('M',1000);
+        int ans=0;
+        for(int i=0;i<s.length();i++){
+            if(i<s.length()-1 && mpp.get(s.charAt(i))<mpp.get(s.charAt(i+1))){
+                ans-=mpp.get(s.charAt(i));
+            }
+            else{
+                ans+=mpp.get(s.charAt(i));
+            }
+        }
+        return ans;
+    }
+}
