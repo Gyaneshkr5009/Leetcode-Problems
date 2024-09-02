84bfa2810b14ae5165d61ef9e301ad8e3aab2d6a
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-09-02 19:26:46 +0530

    Update sort-the-people(2418).java

--- Arrays/sort-the-people(2418).java
+++ Arrays/sort-the-people(2418).java
@@ -17,3 +17,33 @@ class Solution {
         return ans;
     }
 }
+
+//*******************************************(optimal approach)******************************************************
+//time cmplexity:O(n);
+//space complexity:O(n);
+
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        String[] ans=new String[names.length];
+        int idx=0;
+        PriorityQueue<Pair> pq=new PriorityQueue<>(
+            (a,b) ->  b.height-a.height // sort the order in decresing;
+        );
+        for(int i=0;i<names.length ;i++){
+            pq.offer(new Pair(names[i],heights[i]));
+        }
+        while(!pq.isEmpty()){
+            ans[idx++]=pq.poll().name;
+        }
+        return  ans;
+
+    }
+}
+class Pair{
+    String name;
+    int height;
+    Pair(String name ,int height){
+        this.name=name;
+        this.height=height;
+    }
+}
