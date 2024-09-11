0e9a5ef78bb726f486d9816dfc8aae71a480a9b1
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-09-12 01:54:14 +0530

    Update recover-BST(99).java

--- Binary-Search-Tree/recover-BST(99).java
+++ Binary-Search-Tree/recover-BST(99).java
@@ -14,45 +14,24 @@
  * }
  */
 class Solution {
-    TreeNode first;
-    TreeNode second;
-    TreeNode third;
-    TreeNode prev;
     public void recoverTree(TreeNode root) {
-        prev=new TreeNode(Integer.MIN_VALUE);
-        first=second=third=null;
-        helper(root);
-        if(first!=null && third!=null){
-            int temp=first.val;
-            first.val=third.val;
-            third.val=temp;
-        }
-        else if(first!=null && second!=null){
+        if(root==null) return ;
+        inorder(root);
+        if(first!=null && second!=null){
             int temp=first.val;
             first.val=second.val;
             second.val=temp;
         }
-        
     }
-
-    public void helper(TreeNode root){
-        if(root==null){
-            return;
-        }
-        helper(root.left);
+    private TreeNode prev=null , first=null , second=null;
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
         if(prev!=null && prev.val>root.val){
-            if(first==null){
-                first=prev;
-                second=root;
-            }
-            else{
-                third=root;
-            }
+            if(first==null) first=prev;
+            second=root;
         }
         prev=root;
-        helper(root.right);
+        inorder(root.right);
     }
-
-    
-
 }
