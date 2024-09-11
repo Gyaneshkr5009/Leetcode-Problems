4ba9a0fb39b7ea4b08a2a9cd3c65a95aad1fc123
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-09-12 00:37:18 +0530

    Update inorder-Successor-BST(285).java

--- Binary-Search-Tree/inorder-Successor-BST(285).java
+++ Binary-Search-Tree/inorder-Successor-BST(285).java
@@ -22,3 +22,29 @@ class Solution
              inorder(root.right);
          }
 }
+
+
+//*******************************************************(optimal approach)*******************************************************
+//time complexity:O(n);
+//space complexity:O(1);
+
+class Solution
+{
+    // returns the inorder successor of the Node x in BST (rooted at 'root')
+	public Node inorderSuccessor(Node root,Node x)
+         {
+          if(root==null) return root;
+          inorder(root ,x);
+          return ans;
+         }
+         
+         private Node ans;
+         private void inorder(Node root,Node x){
+             if(root==null) return;
+             inorder(root.left,x);
+             if(root.data>x.data){
+                 if(ans==null) ans=root;
+             }
+             inorder(root.right,x);
+         }
+}
