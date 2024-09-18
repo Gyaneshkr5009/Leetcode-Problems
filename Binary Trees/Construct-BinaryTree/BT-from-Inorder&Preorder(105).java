c22544a6519dd5a65876eb777aefd0a05344ad0c
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-09-18 21:43:46 +0530

    Update BT-from-Inorder&Preorder(105).java

--- Binary Trees/Construct-BinaryTree/BT-from-Inorder&Preorder(105).java
+++ Binary Trees/Construct-BinaryTree/BT-from-Inorder&Preorder(105).java
@@ -1,3 +1,14 @@
+/*
+    In an inorder traversal, the sequence is [left → root → right], and in preorder traversal, it's [root → left → right].
+    
+    To construct the binary tree:
+    
+    Take the first element from the preorder array (this is the root).
+    Find the position of this root element in the inorder array.
+    The elements to the left of the root in the inorder array form the left subtree, and the elements to the right form the right subtree.
+    Recursively repeat the process for both left and right subtrees, using the next elements from the preorder array.
+*/
+//**************************************************************************************************************************
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
