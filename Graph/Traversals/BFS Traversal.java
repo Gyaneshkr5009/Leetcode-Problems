031f23706ea220491fe909e7f88a1872ecb8c068
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-09-25 02:55:49 +0530

    Update BFS Traversal.java

--- Graph/Traversals/BFS Traversal.java
+++ Graph/Traversals/BFS Traversal.java
@@ -1,3 +1,6 @@
+//time complexity:O(n) + (2*E)   where 2*E is the total degree of bfs , why total degreee ? becasue inner for loop looping for degreeth time of element;
+//space complexity:O(3*n)
+//*******************************************************************************************************************************************
 class Solution {
     // Function to return Breadth First Traversal of given graph.
     public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
