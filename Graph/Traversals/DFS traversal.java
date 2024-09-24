16fc41dae4341e828c6923907f3fc73aeec0cccd
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-09-25 03:39:38 +0530

    Update DFS traversal.java

--- Graph/Traversals/DFS traversal.java
+++ Graph/Traversals/DFS traversal.java
@@ -1,6 +1,6 @@
 //time complexity:O(n) + O(2*E);         where 2*E is the total degree of graph
 //space complexity:O(3*n);
-//*********************************************************************************************************************************
+//*********************************************************(stack approach)************************************************************************
 class Solution {
     // Function to return a list containing the DFS traversal of the graph.
     public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
@@ -22,5 +22,30 @@ class Solution {
             }
         }
         return list;
+    }
+}
+
+
+//*******************************************************(most used Recursion approach)********************************************************************
+//time complexity : same as above
+//space complexity : O(2n)
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited=new boolean[V];
+        ArrayList<Integer> dfs=new ArrayList<>();
+        visited[0]=true;
+        Dfs(0 , visited , adj , dfs);
+        return dfs;
+    }
+    public void Dfs(int node,boolean[] visited ,  ArrayList<ArrayList<Integer>> adj , ArrayList<Integer> ls){
+        visited[node]=true;
+        ls.add(node);
+        for(int num : adj.get(node)){
+            if(visited[num] == false){
+                Dfs(num , visited , adj , ls);
+            }
+        }
     }
 }
