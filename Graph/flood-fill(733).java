6ccf6c52ae19f846675898d4bce2b163aad64d17
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-09-26 02:14:19 +0530

    Update flood-fill(733).java

--- Graph/flood-fill(733).java
+++ Graph/flood-fill(733).java
@@ -1,6 +1,43 @@
+//time complexity:O(n)
+//space complexity:O(n)
+
+//***************************************************************************(optimal approach dfs traversal)*******************************************
+
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image.length==0) return new int[0][0];
+
+        int preColor=image[sr][sc];
+        if(preColor==color) return image;
+
+        dfs(image , sr, sc , color , preColor);
+        return image;
+    }
+
+    private void dfs(int[][] image , int row , int col , int color , int preColor){
+        //if row and col exceeds then stop;
+        if (row < 0 || row >= image.length || col < 0 || col >= image[0].length) return;
+        //image color !=precolor dont push stop;
+        if(image[row][col]!=preColor) return ;
+        image[row][col]=color;
+
+        //all neighbours of that index there are four(top,right,bottom ,left);
+        int[] dRow={-1 ,0 , 1 ,0};
+        int[] dCol={0 , 1 , 0 , -1};
+
+        for(int i=0 ;i<4 ;i++){
+            int nRow=row+dRow[i];
+            int nCol=col+dCol[i];
+            dfs(image , nRow , nCol , color , preColor);
+        }
+    }
+}
+
+
+//***********************************************************************************************************************************************************************
 //time complexity:O(n);
 //space complexity:O(n)
-//*********************************************************************************************************************************************
+//******************************************************************(bfs traversal)***************************************************************************
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n  =image.length;
