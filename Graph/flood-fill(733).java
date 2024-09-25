f3ddbeb0df15cd23c737c30ee81db6bb75672797
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-09-26 01:54:17 +0530

    Create flood-fill(733).java

---
+++ Graph/flood-fill(733).java
@@ -0,0 +1,45 @@
+//time complexity:O(n);
+//space complexity:O(n)
+//*********************************************************************************************************************************************
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int n  =image.length;
+        int m=image[0].length;
+
+        if(image==null || image.length==0) return new int[0][0]; //some exceptions;
+        int preColor=image[sr][sc];
+        //if preColor =color then dont  need to change anything it will bring no effect so return image;
+        if(preColor==color) return image;
+        image[sr][sc]=color;
+
+
+        //bfs traversal;
+        Queue<Pair> q=new LinkedList<>();
+        q.offer(new Pair(sr ,sc));
+
+        int[] dRow={-1 , 0 , 1 ,0};
+        int[] dCol={0 , 1, 0 , -1};
+        while(!q.isEmpty()){
+            Pair pair=q.poll();
+            int row=pair.row;
+            int col=pair.col;
+            
+            for(int i=0 ;i<4 ;i++){
+                int nRow=row + dRow[i];
+                int nCol=col + dCol[i];
+                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && image[nRow][nCol]==preColor){
+                    image[nRow][nCol]=color;
+                    q.offer(new Pair(nRow , nCol));
+                }
+            }
+        }
+        return image;
+    }
+}
+class Pair{
+    int row , col;
+    Pair(int row , int col){
+        this.row=row;
+        this.col=col;
+    }
+}
