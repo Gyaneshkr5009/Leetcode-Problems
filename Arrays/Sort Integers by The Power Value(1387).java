dc03605b7222bf2282b1101c8d5eda5dd4663788
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2024-12-13 19:07:10 +0530

    Create Sort Integers by The Power Value(1387).java

---
+++ Arrays/Sort Integers by The Power Value(1387).java
@@ -0,0 +1,34 @@
+//*******************************************************************(min Heap)**********************************************************************
+class Solution {
+    public int getKth(int lo, int hi, int k) {
+        if(lo>hi) return -1;
+
+        Pair[] order=new Pair[hi-lo+1];
+        int idx=0;
+        for(int i=lo ; i<=hi ;i++){
+            int num=i;
+            int score=0;
+            while(num!=1){
+                if(num%2==0){
+                    num/=2;
+                }
+                else if(num%2==1){
+                    num=num*3+1;
+                }
+                score++;
+            }
+            order[idx++]=new Pair(i , score);
+        }
+
+        Arrays.sort(order , (a,b)-> a.score!=b.score ? a.score-b.score : a.value-b.value);
+        return order[k-1].value;
+    }
+}
+class Pair{
+    int value;
+    int score;
+    Pair(int value , int score){
+        this.value=value;
+        this.score=score;
+    }
+}
