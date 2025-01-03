89413bf9b68dcbb88640790837d1a160b7050432
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2025-01-03 20:34:19 +0530

    Create  Partition Array Into Three Parts With Equal Sum(1013).java

---
+++ Arrays/Partition Array Into Three Parts With Equal Sum(1013).java
@@ -0,0 +1,32 @@
+//time complexity:O(n);
+//space complexity:O(1);
+//*******************************************************************************************************************
+class Solution {
+    public boolean canThreePartsEqualSum(int[] arr) {
+        int sum = 0;
+        for (int n: arr) {
+            sum += n;
+        }
+
+        if (sum % 3 != 0) {
+            return false;
+        }
+
+        int target = sum / 3;
+        int currentSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            currentSum += arr[i];
+            if (currentSum == target) {
+                currentSum = 0;
+                count++;
+                if (count == 2) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
