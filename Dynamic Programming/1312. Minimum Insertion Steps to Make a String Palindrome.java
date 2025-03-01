80ff42eae473e550842188317726199f9ab56d47
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2025-03-01 14:17:33 +0530

    Update 1312. Minimum Insertion Steps to Make a String Palindrome.java

--- Dynamic Programming/1312. Minimum Insertion Steps to Make a String Palindrome.java
+++ Dynamic Programming/1312. Minimum Insertion Steps to Make a String Palindrome.java
@@ -17,3 +17,23 @@ class Solution {
         return dp[idx1][idx2];
     }
 }
+
+//**********************************************************(optimized reducing String overhead)******************
+class Solution {
+    public int minInsertions(String s) {
+        char[] arr=s.toCharArray();
+        char[] reverse =new StringBuilder(s).reverse().toString().toCharArray();
+        int n=s.length();
+        int[][] dp= new int[n+1][n+1];
+        
+        for(int i=1;i<=n ;i++){
+            for(int j=1 ;j<=n ;j++){
+                if(arr[i-1] == reverse[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else dp[i][j]=Math.max(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        return n- dp[n][n];
+    }
+}
