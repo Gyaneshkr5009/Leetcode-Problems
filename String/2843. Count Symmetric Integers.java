02bf9ac048dfdbe79bf67318a519dba07a433b7c
gyanesh kumar <142999781+Gyaneshkr5009@users.noreply.github.com>
2025-04-11 11:58:24 +0530

    Create 2843. Count Symmetric Integers.java

---
+++ String/2843. Count Symmetric Integers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countSymmetricIntegers(int low, int high) {
+        int cnt=0;
+        for(int i=low;i<=high;i++){
+            //converting the number into String
+            StringBuilder sb=new StringBuilder(String.valueOf(i));
+            //if digits in a number not divisible by two means not a symmatric , so move on to next number
+            if(sb.length()%2!=0) continue;
+            int left=0 , right=sb.length()-1;
+            int sum=0 ,revSum=0;
+            while(left<right){
+                sum+=sb.charAt(left++);
+                revSum+=sb.charAt(right--);
+            }
+            if(sum==revSum) cnt++;
+        }
+        return cnt;
+    }
+}
